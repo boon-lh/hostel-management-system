2b7d4e7cdc42d27ce2b6af5d5211103a78ea5eb1
Jiayuuuuuuuuu <preveenong@gmail.com>
2025-06-20 03:15:02 +0800

    Update visitors.php
    Added sort functions in the table

--- admin/visitors.php
+++ admin/visitors.php
@@ -59,15 +59,14 @@ require_once 'sidebar-admin.php';
             </div>
         </div>
         <div class="card-content">
-            <div class="table-responsive">
-                <table class="data-table" id="visitors-table">
+            <div class="table-responsive">                <table class="data-table" id="visitors-table">
                     <thead>
                         <tr>
-                            <th>Visitor Name</th>
+                            <th class="sortable" data-sort="name">Visitor Name <i class="fas fa-sort"></i></th>
                             <th>Phone Number</th>
-                            <th>Gender</th>
+                            <th class="sortable" data-sort="gender">Gender <i class="fas fa-sort"></i></th>
                             <th>IC Number</th>
-                            <th>Visit Date</th>
+                            <th class="sortable" data-sort="date">Visit Date <i class="fas fa-sort"></i></th>
                             <th>Time In</th>
                             <th>Time Out</th>
                             <th>Room Number</th>
@@ -76,13 +75,12 @@ require_once 'sidebar-admin.php';
                     </thead>
                     <tbody>
                         <?php if (count($visitors) > 0): ?>
-                            <?php foreach ($visitors as $visitor): ?>
-                                <tr>
-                                    <td><?php echo htmlspecialchars($visitor['name']); ?></td>
+                            <?php foreach ($visitors as $visitor): ?>                                <tr>
+                                    <td data-value="<?php echo htmlspecialchars($visitor['name']); ?>"><?php echo htmlspecialchars($visitor['name']); ?></td>
                                     <td><?php echo htmlspecialchars($visitor['contact_no']); ?></td>
-                                    <td><?php echo htmlspecialchars($visitor['gender']); ?></td>
+                                    <td data-value="<?php echo htmlspecialchars($visitor['gender']); ?>"><?php echo htmlspecialchars($visitor['gender']); ?></td>
                                     <td><?php echo htmlspecialchars($visitor['ic_number']); ?></td>
-                                    <td><?php echo date('d M Y', strtotime($visitor['visit_date'])); ?></td>
+                                    <td data-value="<?php echo $visitor['visit_date']; ?>"><?php echo date('d M Y', strtotime($visitor['visit_date'])); ?></td>
                                     <td><?php echo date('h:i A', strtotime($visitor['time_in'])); ?></td>
                                     <td>
                                         <?php 
@@ -109,15 +107,80 @@ require_once 'sidebar-admin.php';
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
+<style>
+    /* Style for sortable columns */
+    th.sortable {
+        cursor: pointer;
+        position: relative;
+    }
+    th.sortable i.fas {
+        margin-left: 5px;
+        font-size: 0.8em;
+        color: #aaa;
+    }
+    th.sortable.asc i.fas:before {
+        content: "\f0de"; /* fa-sort-up */
+        color: #333;
+    }
+    th.sortable.desc i.fas:before {
+        content: "\f0dd"; /* fa-sort-down */
+        color: #333;
+    }
+</style>
+
 <script>
-    // Simple search functionality for visitors
+    // Search and sorting functionality for visitors table
     $(document).ready(function() {
+        // Search functionality
         $("#visitor-search").on("keyup", function() {
             var value = $(this).val().toLowerCase();
             $("table tbody tr").filter(function() {
                 $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
             });
         });
+        
+        // Sorting functionality
+        $("th.sortable").on("click", function() {
+            var table = $(this).parents("table").eq(0);
+            var rows = table.find("tr:gt(0)").toArray().sort(comparer($(this).index()));
+            var sortDir = this.asc = !this.asc ? true : !this.asc;
+            
+            // Update sort icons
+            $("th.sortable").removeClass("asc desc");
+            $(this).addClass(sortDir ? "asc" : "desc");
+            
+            // Sort rows
+            if (!sortDir) {
+                rows = rows.reverse();
+            }
+            
+            // Append sorted rows to table
+            for (var i = 0; i < rows.length; i++) {
+                table.append(rows[i]);
+            }
+        });
+        
+        // Comparison function for sorting
+        function comparer(index) {
+            return function(a, b) {
+                var valA = getCellValue(a, index);
+                var valB = getCellValue(b, index);
+                
+                // Handle date comparison
+                if ($("th").eq(index).data("sort") === "date") {
+                    valA = new Date(valA);
+                    valB = new Date(valB);
+                }
+                
+                return $.isNumeric(valA) && $.isNumeric(valB) ? 
+                    valA - valB : valA.localeCompare(valB);
+            };
+        }
+        
+        // Get cell value for sorting
+        function getCellValue(row, index) {
+            return $(row).children('td').eq(index).text().trim();
+        }
     });
 </script>
 </body>
